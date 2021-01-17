a472377f2e352b290b6e6652d7895f528e921239
VyaasManjukumar <73779392+VyaasManjukumar@users.noreply.github.com>
2021-01-17 17:09:15 +0000

    Add files via upload

---
+++ chain.js
@@ -0,0 +1,25 @@
+class CHAIN{
+  constructor(pointA,bodyB) {
+
+      var options = {
+          pointA:pointA,
+          bodyB:bodyB,
+          length: 350,
+          stiffness : 0.04
+        
+
+          
+      }
+      this.pointA = pointA
+      this.chain=Constraint.create(options);
+      World.add(world, this.chain);
+    }
+    display(){
+      var pointA = this.pointA;
+      var pointB = this.chain.bodyB.position;
+
+      strokeWeight(4);
+      stroke("white")
+      line(pointA.x,pointA.y,pointB.x,pointB.y);
+    }
+}
